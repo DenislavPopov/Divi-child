05dd6991ab383ebf54bcac19509bc06e46d7e2c9
Denislav23 <denislavppv@gmail.com>
2017-10-18 16:47:42 +0300

    Add files via upload

---
+++ assets/js/custom/custom.js
@@ -0,0 +1,7 @@
+console.log('%c Loaded and Ready for cool stuff', 'background: #F0F0F0; color: #222222;');
+
+/* Sample simple animation of fading menu*/
+/*
+var menu = document.getElementById("et-top-navigation");
+TweenMax.fromTo(menu, 2, {opacity: 0}, {opacity: 1});
+*/
